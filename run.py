8ff9f582816f200faaad212d52b7c69e68a90f2c
Brett Hobbs <46791301+BrettTheHobbit@users.noreply.github.com>
2022-12-02 20:40:23 -0500

    tweaks

--- run.py
+++ run.py
@@ -21,13 +21,8 @@ ROW = [1, 2, 3, 4, 5, 6]
 COL = [1, 2, 3, 4, 5]
 STATUS = ["CORRECT", "INCORRECT", "PARTIAL", "EMPTY"]
 POSSIBLE_LETTER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "]
-POSSIBLE_LETTER1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-POSSIBLE_LETTER2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-POSSIBLE_LETTER3 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-POSSIBLE_LETTER4 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-POSSIBLE_LETTER5 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 BOARD = WLR.Wordle_board("words.txt", 15)
-guess_list = BOARD.get_guess_list()
+
 
 
 PROPOSITIONS = []
@@ -103,102 +98,103 @@ class Word:
 #  what the expectations are.
 def wordle_theory():
    #Adds constraints
-#BASIC CONSTRAINTS
+#BASIC CONSTRAINTS 
+    guessed_list = BOARD.get_guess_list()
+    print("guess list idk" + str(guessed_list))
 
+    if(len(guessed_list) > 1):
 #Each row maps to every column
-    for row in ROW:
-        for col in COL:
-            constraint.add_at_most_one(E, Slots(row, col))
+        for row in ROW:
+            for col in COL:
+                constraint.add_at_most_one(E, Slots(row, col))
 
-    #For each slot, there is exactly one status applied to it
-    for row in ROW:
-        for col in COL:
-            constraint.add_at_most_one(E, [SlotStatus(row, col, status) for status in STATUS]) #Get char from the char at the index (Fang Lei's class)
+        #For each slot, there is exactly one status applied to it
+        for row in ROW:
+            for col in COL:
+                constraint.add_at_most_one(E, [SlotStatus(row, col, status) for status in STATUS]) #Get char from the char at the index (Fang Lei's class)
 
-    #For each slot, there is exactly one letter applied to it
-    for row in ROW:
-        for col in COL:
-            constraint.add_at_most_one(E, [SlotLetter(row, col, letter) for letter in POSSIBLE_LETTER])
+        #For each slot, there is exactly one letter applied to it
+        for row in ROW:
+            for col in COL:
+                constraint.add_at_most_one(E, [SlotLetter(row, col, letter) for letter in POSSIBLE_LETTER])
 
-    #Each letter has a position and a status (by adding two constraints, the result should be the intersection of them)
-    for row in ROW:
-        for col in COL:
-            for letter in POSSIBLE_LETTER:
-                constraint.add_at_most_one(E, [Guess(row, col, status, letter) for status in STATUS])
+        #Each letter has a position and a status (by adding two constraints, the result should be the intersection of them)
+        for row in ROW:
+            for col in COL:
+                for letter in POSSIBLE_LETTER:
+                    constraint.add_at_most_one(E, [Guess(row, col, status, letter) for status in STATUS])
 
-    for row in ROW:
-        for col in COL:
-            for status in STATUS:
-                constraint.add_at_most_one(E, [Guess(row, col, status, letter) for letter in POSSIBLE_LETTER])
+        for row in ROW:
+            for col in COL:
+                for status in STATUS:
+                    constraint.add_at_most_one(E, [Guess(row, col, status, letter) for letter in POSSIBLE_LETTER])
 
-    #ADVANCED CONSTRAINTS
+        #ADVANCED CONSTRAINTS
 
-    #If the letter is blank then the status must be empty and vice versa
-    for row in ROW:
-        for col in COL:
-            E.add_constraint(SlotLetter(row, col, "BLANK") >> SlotStatus(row, col, STATUS[3]))
+        #If the letter is blank then the status must be empty and vice versa
+        for row in ROW:
+            for col in COL:
+                E.add_constraint(SlotLetter(row, col, "BLANK") >> SlotStatus(row, col, STATUS[3]))
 
-    for row in ROW:
-        for col in COL:
-            E.add_constraint(SlotStatus(row, col, STATUS[3]) >> SlotLetter(row, col, "BLANK"))
+        for row in ROW:
+            for col in COL:
+                E.add_constraint(SlotStatus(row, col, STATUS[3]) >> SlotLetter(row, col, "BLANK"))
 
-    #If the letter is partial, it is correct in the  one of the other four slots
-    for row in ROW:
-        for p in POSSIBLE_LETTER:
-            E.add_constraint(Guess(row, 1, STATUS[2], p) >> (Guess(row, 2, STATUS[0], p) | Guess(row, 3, STATUS[0], p) | Guess(row, 4, STATUS[0], p) | Guess(row, 5, STATUS[0], p)))
-            E.add_constraint(Guess(row, 2, STATUS[2], p) >> (Guess(row, 1, STATUS[0], p) | Guess(row, 3, STATUS[0], p) | Guess(row, 4, STATUS[0], p) | Guess(row, 5, STATUS[0], p)))
-            E.add_constraint(Guess(row, 3, STATUS[2], p) >> (Guess(row, 2, STATUS[0], p) | Guess(row, 1, STATUS[0], p) | Guess(row, 4, STATUS[0], p) | Guess(row, 5, STATUS[0], p)))
-            E.add_constraint(Guess(row, 4, STATUS[2], p) >> (Guess(row, 2, STATUS[0], p) | Guess(row, 3, STATUS[0], p) | Guess(row, 1, STATUS[0], p) | Guess(row, 5, STATUS[0], p)))
-            E.add_constraint(Guess(row, 5, STATUS[2], p) >> (Guess(row, 2, STATUS[0], p) | Guess(row, 3, STATUS[0], p) | Guess(row, 4, STATUS[0], p) | Guess(row, 1, STATUS[0], p)))
+        for row in ROW:
+            for word in guessed_list:
+                E.add_constraint(Guess(row, 1, STATUS[2], word[0]) >> (Guess(row, 2, STATUS[0], word[1]) | Guess(row, 3, STATUS[0], word[2]) | Guess(row, 4, STATUS[0], word[3]) | Guess(row, 5, STATUS[0], word[4])))
+                E.add_constraint(Guess(row, 2, STATUS[2], word[1]) >> (Guess(row, 1, STATUS[0], word[0]) | Guess(row, 3, STATUS[0], word[2]) | Guess(row, 4, STATUS[0], word[3]) | Guess(row, 5, STATUS[0], word[4])))
+                E.add_constraint(Guess(row, 3, STATUS[2], word[2]) >> (Guess(row, 2, STATUS[0], word[1]) | Guess(row, 1, STATUS[0], word[0]) | Guess(row, 4, STATUS[0], word[3]) | Guess(row, 5, STATUS[0], word[4])))
+                E.add_constraint(Guess(row, 4, STATUS[2], word[3]) >> (Guess(row, 2, STATUS[0], word[1]) | Guess(row, 3, STATUS[0], word[2]) | Guess(row, 1, STATUS[0], word[0]) | Guess(row, 5, STATUS[0], word[4])))
+                E.add_constraint(Guess(row, 5, STATUS[2], word[4]) >> (Guess(row, 2, STATUS[0], word[1]) | Guess(row, 3, STATUS[0], word[2]) | Guess(row, 4, STATUS[0], word[3]) | Guess(row, 1, STATUS[0], word[0])))
 
-    #If there are five correct letters, then the word is correct
-    for row in ROW:
-        for p in POSSIBLE_LETTER:
-            E.add_constraint((Guess(row, 1, STATUS[0], p) & Guess(row, 2, STATUS[0], p) & Guess(row, 3, STATUS[0], p) & Guess(row, 4, STATUS[0], p) & Guess(row, 5, STATUS[0], p)) >> Word(Guess(row, 1, STATUS[0], p).letter + Guess(row, 2, STATUS[0], p).letter + Guess(row, 3, STATUS[0], p).letter + Guess(row, 4, STATUS[0], p).letter + Guess(row, 5, STATUS[0], p).letter, True))
+        #If there are five correct letters, then the word is correct
+        for row in ROW:
+            for word in guessed_list:
+                E.add_constraint((Guess(row, 1, STATUS[0], word[0]) & Guess(row, 2, STATUS[0], word[1]) & Guess(row, 3, STATUS[0], word[2]) & Guess(row, 4, STATUS[0], word[3]) & Guess(row, 5, STATUS[0], word[4])) >> Word(word, True))
+        #SOLVING CONSTRAINTS
 
-    #SOLVING CONSTRAINTS
+        #If we run out of letters in letter bank allow duplicate letters 
 
-    #If we run out of letters in letter bank allow duplicate letters 
+        #IF there are 5 partially correct letter and totally correct letters, start trying to guess the word
 
-    #IF there are 5 partially correct letter and totally correct letters, start trying to guess the word
+        #If the letter is not in the correct word,then all words in the wordlist with incorrect letters should be removed
+        #If the letter is not in the correct word,then all words in the wordlist with incorrect letters should be removed
+        for row in ROW:
+            for word in guessed_list:
+                E.add_constraint((Guess(row, 1, STATUS[1], word[0]) & Guess(row, 2, STATUS[1], word[1]) & Guess(row, 3, STATUS[1], word[2]) & Guess(row, 4, STATUS[1], word[3]) & Guess(row, 5, STATUS[1], word[4])) >> Word(word, False))
+                if(Word(word, False)== True):
+                    remove_word_list = []
+                    current_list_size = range(len(guessed_list))
+                    for x in current_list_size:
+                        if re.search([word[0],word[1],word[2],word[3],word[4]],x.upper()) == True:
+                            remove_word_list.append(x)
+                    BOARD.remove_invalid(remove_word_list)
+                    guessed_list = BOARD.get_guess_list()
+        #If the correct letter is in the right position, then remove all words without the correct letter from word list
+            #Check if the correct letter is in the correct position
+        def remove_word(col, letter):
+            remove_word_list = guessed_list.copy()
+            for x in range(len(guessed_list)):
+                if x[col] == letter:
+                    remove_word_list.remove(x)
+            BOARD.remove_invalid(remove_word_list)
+            guessed_list = BOARD.get_guess_list()
 
-    #If the letter is not in the correct word,then all words in the wordlist with incorrect letters should be removed
-    for row in ROW:
-        for p in POSSIBLE_LETTER:
-            full_word = Guess(row, 1, STATUS[1], p).letter + Guess(row, 2, STATUS[1], p).letter + Guess(row, 3, STATUS[1], p).letter + Guess(row, 4, STATUS[1], p).letter + Guess(row, 5, STATUS[1], p).letter
-            E.add_constraint((Guess(row, 1, STATUS[1], p) & Guess(row, 2, STATUS[1], p) & Guess(row, 3, STATUS[1], p) & Guess(row, 4, STATUS[1], p) & Guess(row, 5, STATUS[1], p)) >> Word(full_word, False))
-            if(Word(full_word, False)== True):
-                remove_word_list = []
-                current_list_size = range(len(guess_list))
-                for x in current_list_size:
-                    if re.search([full_word[0],full_word[1],full_word[2],full_word[3],full_word[4]],x.upper()) == True:
-                        remove_word_list.append(x)
-                guess_list = BOARD.remove_invalid(remove_word_list)
-    
-    #If the correct letter is in the right position, then remove all words without the correct letter from word list
-        #Check if the correct letter is in the correct position
-    def remove_word(col, letter):
-        remove_word_list = guess_list.copy()
-        for x in range(len(guess_list)):
-            if x[col] == letter:
-                remove_word_list.remove(x)
-        guess_list = BOARD.remove_invalid(remove_word_list)
 
-
-    for row in ROW:
-        for col in COL:
-            for letter in POSSIBLE_LETTER:
-                exclusion_list = POSSIBLE_LETTER.copy().remove(letter)
-                #E.add_constraint(SlotStatus(row, col, STATUS[0]) & SlotLetter(row, col letter) >> )
-                E.add_constraint(Guess(row, col, STATUS[0], letter) >> ~(Guess(row, col, STATUS[0], exclusion_list)))
-                if(Guess(row, col, STATUS[0], letter) == True):
-                    remove_word(col, letter)
-
-    return E
+        for row in ROW:
+            for col in COL:
+                for letter in POSSIBLE_LETTER:
+                    exclusion_list = POSSIBLE_LETTER.copy().remove(letter)
+                    #E.add_constraint(SlotStatus(row, col, STATUS[0]) & SlotLetter(row, col letter) >> )
+                    E.add_constraint(Guess(row, col, STATUS[0], letter) >> ~(Guess(row, col, STATUS[0], exclusion_list)))
+                    if(Guess(row, col, STATUS[0], letter) == True):
+                        remove_word(col, letter)
+    else:
+        return E
 
 
 if __name__ == "__main__":
-
     T = wordle_theory()
     # Don't compile until you're finished adding all your constraints!
     T = E.compile()
